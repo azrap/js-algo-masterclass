7ea1b7e71b0b9410643f56b5f230a5b779f62cfa
azrap <azraap@gmail.com>
2019-10-24 12:21:53 -0700

    bubble sort optimized

--- sortAlgos/bubbleSort.js
+++ sortAlgos/bubbleSort.js
@@ -1,11 +1,15 @@
 function bubbleSort(arr) {
   for (var i = arr.length - 1; i >= 0; i--) {
+    noSwaps = true;
     for (var j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
+        noSwaps = false;
       }
       console.log("swapped arr:", arr);
     }
+
+    if (noSwaps) break;
   }
   return arr;
   // loop through the array, take the nth element and compare to each element in the array
@@ -20,4 +24,4 @@ const swap = (arr, idx1, idx2) => {
   return arr;
 };
 
-console.log([10, 4, 6, 8, 2], "bubba sorted:", bubbleSort([10, 4, 6, 8, 2]));
+console.log([8, 1, 2, 3, 4], "bubba sorted:", bubbleSort([8, 1, 2, 3, 4]));
